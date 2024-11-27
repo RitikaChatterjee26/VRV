53f5c50cfef8f740f1b9cc7ff1d0089cb3e638f0
RitikaChatterjee26 <155581960+RitikaChatterjee26@users.noreply.github.com>
2024-11-27 19:32:25 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -4,19 +4,19 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const router = express.Router();
 
-// Register (Signup)
+
 router.post('/signup', async (req, res) => {
     const { name, email, password, role } = req.body;
 
     try {
-        // Check if the user already exists
+        
         const userExists = await User.findOne({ email });
         if (userExists) return res.status(400).json({ message: 'User already exists' });
 
         const user = new User({ name, email, password, role });
         await user.save();
 
-        // Create JWT token
+        
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
             expiresIn: '1h',
         });
@@ -27,7 +27,6 @@ router.post('/signup', async (req, res) => {
     }
 });
 
-// Login
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
@@ -35,11 +34,11 @@ router.post('/login', async (req, res) => {
         const user = await User.findOne({ email });
         if (!user) return res.status(404).json({ message: 'User not found' });
 
-        // Validate password
+        
         const isMatch = await user.matchPassword(password);
         if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
 
-        // Create JWT token
+        
         const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
             expiresIn: '1h',
         });
